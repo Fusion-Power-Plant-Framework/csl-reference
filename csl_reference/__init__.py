d1f8ff945d2b2b86e74146ab991d809c6767751f
je-cook <81617086+je-cook@users.noreply.github.com>
2025-08-27 11:42:58 +0100

    Be considerate of logging as library code (#4)

--- csl_reference/__init__.py
+++ csl_reference/__init__.py
@@ -3,6 +3,16 @@
 # SPDX-License-Identifier: MIT
 """Package to represent references."""
 
+import logging
+
 from csl_reference._reference import DateVariable, NameVariable, Reference, ReferenceType
 
-__all__ = ["DateVariable", "NameVariable", "Reference", "ReferenceType"]
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+__all__ = [
+    "DateVariable",
+    "NameVariable",
+    "Reference",
+    "ReferenceType",
+]
